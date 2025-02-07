5f831df0e040922986996167bc5e964ba6883330
S. Gopi Tejeshwar Reddy <iamnani23042006@gmail.com>
2025-02-07 22:37:10 +0530

    Add files via upload

---
+++ basics/if else.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+ int n;
+ cin>>n;
+  string names[n];
+ cout<<"taking input"<<endl;
+ for(int i=0;i<n;i++){
+    cin>>names[i];
+ }
+ cout<<"output:"<<endl;
+ for(int i=0;i<n;i++){
+    cout<<names[i]<<endl;
+
+ }
+}
+
