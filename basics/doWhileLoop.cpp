aff1070a8492bd1fc5e198049e8c944e03ea3c80
S. Gopi Tejeshwar Reddy <iamnani23042006@gmail.com>
2025-02-08 21:22:24 +0530

    Add files via upload

---
+++ basics/doWhileLoop.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // step 1: initialise before loop
+    // step 2: increment inside the loop
+    // step 3: condition with in the brackets of while loop
+
+    int i = 1;
+    do{
+        cout<<"hellow world: "<<i<<endl;
+        i++;
+    }while(i<=10);
+
+
+
+
+
+
+    return 0;
+}
